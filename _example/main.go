9ec60006c08d2dc80820e0020491fed349fbc8c6
wangyj <wangyj@liepin.com>
2017-11-28 10:31:45 +0800

    change package main to nkill

--- _example/main.go
+++ _example/main.go
@@ -1,20 +1,13 @@
-# nkill
+package main
 
-Kills all processes listening on the given TCP ports.
+import (
+	"log"
+	"os"
+	"strconv"
 
-#### Install
+	"github.com/soopsio/nkill"
+)
 
-You need go installed and GOBIN in your PATH. Once that is done, run the command:
-
-```bash
-   $ go get -u github.com/soopsio/nkill
-```
-
-#### Usage
-
-To kill any process listening to the port 8080:
-
-```go
 func main() {
 	if len(os.Args) <= 1 {
 		log.Fatalln("Kills all processes listening on the given TCP ports.\nusage: nkill port")
@@ -34,9 +27,3 @@ func main() {
 	}
 
 }
-
-```
-
-##### Inspiration
-
-http://voorloopnul.com/blog/a-python-netstat-in-less-than-100-lines-of-code/
--- nkill.go
+++ nkill.go
@@ -1,6 +1,6 @@
 // Kills all processes listening on the given TCP ports.
 
-package main
+package nkill
 
 import (
 	"fmt"
@@ -123,7 +123,7 @@ func getProcessExe(pid string) string {
 	return path
 }
 
-func killPort(portToKill int64) {
+func KillPort(portToKill int64) {
 	killed := false
 	for _, conn := range netstat(portToKill) {
 		if err := conn.Kill(); err != nil {
@@ -141,24 +141,4 @@ func killPort(portToKill int64) {
 
 func init() {
 	log.SetFlags(0)
-}
-
-func main() {
-	if len(os.Args) <= 1 {
-		log.Fatalln("Kills all processes listening on the given TCP ports.\nusage: nkill port")
-	}
-
-	// if os.Getpid() != 0 {
-	// 	log.Println("WARNING: You are not running this script as superuser.")
-	// }
-
-	for _, port := range os.Args[1:] {
-		p, err := strconv.ParseInt(port, 10, 64)
-		if err != nil {
-			log.Printf("%s is not a valid port number\n", port)
-			continue
-		}
-		killPort(p)
-	}
-
 }
